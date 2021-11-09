27ee05dfe8e6d5db479496b0a6cb1454086a99d6
manasa525 <85554630+manasa525@users.noreply.github.com>
2021-11-09 14:17:14 +0530

    Add files via upload

---
+++ main1.py.py
@@ -0,0 +1,7 @@
+import math
+number=int(input("enter a number"))
+root=math.sqrt(number)
+if int(root+0.5)**2=number:
+     print(number,"perfect square")
+else:
+     print('number,"not a perfect square")
